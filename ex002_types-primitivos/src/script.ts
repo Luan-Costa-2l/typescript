021558810ac100a991b23f8d842aef042fac57d4
Luan Cordeiro <luam.lc19@gmail.com>
2022-11-11 20:17:43 -0300

    Types primitivos

---
+++ ex002_types-primitivos/src/script.ts
@@ -0,0 +1,7 @@
+// Declaro o tipo de user como sendo string então não posso atribuir um valor number para a variável simplesmente usando user = 4
+let user: string = 'Luan';
+
+// ps: não é obrigatório declarar o tipo da variável sempre, use quando necessário
+let age: number = 90;
+
+let developer: boolean = true;
